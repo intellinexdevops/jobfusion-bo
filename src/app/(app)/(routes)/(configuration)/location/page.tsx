1871dd3dd14d86cf250c52f3a2244811a1c126c3
Chenter <chenterphai61@gmail.com>
2025-06-08 15:41:14 +0700

    ..

--- src/app/(app)/(routes)/(configuration)/location/page.tsx
+++ src/app/(app)/(routes)/(configuration)/location/page.tsx
@@ -2,5 +2,5 @@ import LocationMainPage from '@/components/pages/LocationMainPage'
 import React from 'react'
 
 export default function Page() {
-    return <LocationMainPage/>
+    return <LocationMainPage />
 }
---
+++ src/lib/database/configuration.ts
@@ -0,0 +1,59 @@
+// Copyright 2025 chenterphai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+declare global {
+  // eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
+  var mongoose: any;
+}
+
+import mongoose from "mongoose";
+import config from "../config";
+
+const MONGODB_URI = config.NEXT_PUBLIC_CONFIGURATION_MONGODB_URI;
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Global is used here to maintain a cached connection across hot reloads in development
+let cached: MongooseCache = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
+      console.log("Connected to MongoDB");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
